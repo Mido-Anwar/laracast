e3dc91d46c79f6812e0d8ec78cb9e2475823bd4b
Mido Anwar <mimianwar33@hotmail.com>
2025-06-28 21:31:05 +0300

    i'm back

--- views/pages/status/403.php
+++
@@ -1,7 +0,0 @@
-<main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p style="color:red;"> you are unauthorized </p>
-        <a href="/">GO To Home</a>
-        <!-- Your content -->
-    </div>
-</main>
--- views/pages/status/404.php
+++
@@ -1,7 +0,0 @@
-<main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p style="color:red;"> sorry page not found</p>
-        <a href="/">GO To Home</a>
-        <!-- Your content -->
-    </div>
-</main>
