773b5c50b1268eea4a97189c23928b85840b581e
Mido-Anwar <mimianwar33@hotmail.com>
2026-04-14 20:33:12 +0200

    first commit long ago

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
 
-require "App/config.php";
-require "App/routes.php";
 
-//dd(PAGE_COMPONENT);
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 
 echo "mido";
