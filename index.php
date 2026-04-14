3e52759bf79d5a841f939c0da8b79de7a1bd6242
Mido-Anwar <mimianwar33@hotmail.com>
2026-04-14 20:46:42 +0200

    add folders & files

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laracast php crash course</title>
+    <link rel="stylesheet" href="sass/app.css">
+</head>
 
+<body>
+    <div>
 
+    
+        <?php
 
+        echo "mido";
 
-echo "mido";
+        ?>
+    </div>
+    <script src="js/app.js"></script>
+</body>
 
+</html>
