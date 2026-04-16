8706908d7864048925889576864e12cb97ebf57f
Mido-Anwar <mimianwar33@hotmail.com>
2026-04-16 23:49:16 +0200

    php basics

--- index.php
+++ index.php
@@ -10,18 +10,60 @@
 
 <body>
     <div>
+        <h1>php basics</h1>
+        <div class="holder">
+            <h2>php code space</h2>
+            php code should be written inside
 
-        // this is a comment
-        /* 
-        this is a multi line comment
-        this is a multi line comment
-        this is a multi line comment
-        this is a multi line comment
-        */
-        
-        <?php
-        echo "mido";
-        ?>
+            <code>
+                &lt;?php
+                echo "Welcome to PHP";
+                ?&gt;
+            </code>
+            tags
+            the code will be executed on the server and the result will be sent to the browser
+            the code will not be visible to the user
+            the code will be executed on the server and the result will be sent to the browser
+            the code will not be visible to the user
+            the code will be executed on the server and the result will be sent to the browser
+        </div>
+        <div class="holder">
+            <h2>comments</h2>
+            <p>
+                // this is a one line comment
+                /*
+                this is a multi line comment
+                this is a multi line comment
+                this is a multi line comment
+                this is a multi line comment
+                */</p>
+        </div>
+        <div class="holder">
+            <h2> variabels & data types</h2>
+            <p>
+                $ is used to declare a variable in php and the variable name should start with a letter or an underscore and can be followed by any number of letters, numbers, or underscores
+                the variable name should not be a reserved word in php
+                the variable name should not contain spaces
+                the variable name should not start with a number
+                the variable name should not contain special characters except for underscore
+                the variable name should not be case sensitive
+                the variable name should not be a reserved word in php
+                the variable name should not contain spaces
+                the variable name should not start with a number
+                the variable name should not contain special characters except for underscore
+                the variable name should not be case sensitive
+                the variable name should not be a reserved word in php
+                the variable name should not contain spaces
+                the variable name should not start with a number
+                the variable name should not contain special characters except for underscore
+                $string = "mido";
+                $integer1 = 10;
+                $float1 = 3.14;
+                $boolean1 = true or false;
+                this is identifier a variable that holds a value of type string or integer or float or boolean
+            </p>
+        </div>
+    
     </div>
     <script src="js/app.js"></script>
 </body>
